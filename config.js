9482b2bc8738a05303d589c45caf383e9a23393a
Gogotsmod <96406101+Gogotsmod@users.noreply.github.com>
2022-11-09 17:18:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'GOGOTS BOT'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281223163244','6281223163244','6283846077142']
+global.premium = ['6283846077142','6281223163244']
+global.packname = 'GOGOTS Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
